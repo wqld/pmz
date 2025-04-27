c1c650be2af925288990332cd3d3a7d46aa7887f
Wongyu Lee <kyu21@outlook.com>
2025-04-27 14:09:16 +0000

    fix: add ebpf loader to cni

--- cni/src/main.rs
+++ cni/src/main.rs
@@ -7,6 +7,7 @@ use std::{
 };
 
 use anyhow::{Result, bail};
+use aya::programs::{SchedClassifier, tc};
 use clap::Parser;
 use http_body_util::Full;
 use hyper::{
@@ -28,6 +29,9 @@ use tonic::transport;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
+    #[clap(short, long, default_value = "eth0")]
+    iface: String,
+
     #[arg(short, long, default_value = "/etc/cni/net.d")]
     cni_conf_dir: String,
 }
@@ -50,6 +54,39 @@ async fn main() -> Result<()> {
     let args = Args::parse();
     let namespace = std::env::var("CNI_NAMESPACE")?;
     let host_ip = std::env::var("HOST_IP")?;
+
+    // Bump the memlock rlimit. This is needed for older kernels that don't use the
+    // new memcg based accounting, see https://lwn.net/Articles/837122/
+    let rlim = libc::rlimit {
+        rlim_cur: libc::RLIM_INFINITY,
+        rlim_max: libc::RLIM_INFINITY,
+    };
+    let ret = unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlim) };
+    if ret != 0 {
+        debug!("remove limit on locked memory failed, ret is: {}", ret);
+    }
+
+    // This will include your eBPF object file as raw bytes at compile-time and load it at
+    // runtime. This approach is recommended for most real-world use cases. If you would
+    // like to specify the eBPF program at runtime rather than at compile-time, you can
+    // reach for `Bpf::load_file` instead.
+    let mut ebpf = aya::Ebpf::load(aya::include_bytes_aligned!(concat!(
+        env!("OUT_DIR"),
+        "/pmz"
+    )))?;
+    if let Err(e) = aya_log::EbpfLogger::init(&mut ebpf) {
+        // This can happen if you remove all log statements from your eBPF program.
+        warn!("failed to initialize eBPF logger: {}", e);
+    }
+
+    let iface = args.iface;
+    // error adding clsact to the interface if it is already added is harmless
+    // the full cleanup can be done with 'sudo tc qdisc del dev eth0 clsact'.
+    let _ = tc::qdisc_add_clsact(&iface);
+
+    let interceptor: &mut SchedClassifier = ebpf.program_mut("interceptor").unwrap().try_into()?;
+    interceptor.load()?;
+    interceptor.attach(&iface, tc::TcAttachType::Ingress)?;
 
     // discovery thread
     tokio::spawn(async move {
